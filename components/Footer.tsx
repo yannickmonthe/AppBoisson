466ae14e9517cc8c08846f4927973b4ee925af19
yannickmonthe <mzeboy@icloud.com>
2021-12-14 21:17:18 -0500

    Footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {StyleSheet, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -7,26 +7,96 @@ interface IconProps {
     iconColor: string
 }
 
-function isSelected() {
-    //todo si icon est toucher deviens vert
-}
-
 function IconFooter(props :IconProps){
     return(
-    <TouchableOpacity style={styles.icon}>
-        <Icon name={props.iconName} size={40} color={props.iconColor} />
-    </TouchableOpacity>
+        <Icon style={styles.icon} name={props.iconName} size={40} color={props.iconColor} />
 )}
 
+const whiteColor = 'white';
+const greenColor = "#3ac730";
 
 export default function Footer() {
+
+  const [gridColor, setGridColor] = useState(greenColor);
+  const [cartColor, setCartColor] = useState(whiteColor);
+  const [circleColor, setCircleColor] = useState(whiteColor);
+  const [signalColor, setSignalColor] = useState(whiteColor);
+  const [personColor, setPersonColor] = useState(whiteColor);
+
+  function isSetColor(name: string) {
+    
+
+    switch(name) {
+      case 'grid': {
+        setGridColor(greenColor);
+        setCircleColor(whiteColor);
+        setPersonColor(whiteColor);
+        setCartColor(whiteColor);
+        setSignalColor(whiteColor);
+        break;
+      }
+
+      case 'cart': {
+        setGridColor(whiteColor);
+        setCircleColor(whiteColor);
+        setPersonColor(whiteColor);
+        setCartColor(greenColor);
+        setSignalColor(whiteColor);
+        break;
+      }
+
+      case 'circle': {
+        setGridColor(whiteColor);
+        setCircleColor(greenColor);
+        setPersonColor(whiteColor);
+        setCartColor(whiteColor);
+        setSignalColor(whiteColor);
+        break;
+      }
+
+      case 'cellular': {
+        setGridColor(whiteColor);
+        setCircleColor(whiteColor);
+        setPersonColor(whiteColor);
+        setCartColor(whiteColor);
+        setSignalColor(greenColor);
+        break;
+      }
+
+      case 'person': {
+        setGridColor(whiteColor);
+        setCircleColor(whiteColor);
+        setPersonColor(greenColor);
+        setCartColor(whiteColor);
+        setSignalColor(whiteColor);
+        break;
+      }
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <IconFooter iconName='grid-outline' iconColor='#3ac730'/>
-      <IconFooter iconName='cart-outline' iconColor='white'/>
-      <IconFooter iconName='add-circle' iconColor='white'/>
-      <IconFooter iconName='cellular' iconColor='white'/>
-      <IconFooter iconName='person-outline' iconColor='white'/>
+      
+      <TouchableOpacity onPress={ () => { isSetColor('grid') }}>
+        <IconFooter iconName='grid-outline' iconColor={gridColor}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => { isSetColor('cart') }}>
+        <IconFooter iconName='cart-outline' iconColor={cartColor}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => { isSetColor('circle') }}>
+        <IconFooter iconName='add-circle' iconColor={circleColor}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => { isSetColor('cellular') }}>
+      <IconFooter iconName='cellular' iconColor={signalColor}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => { isSetColor('person') }}>
+        <IconFooter iconName='person-outline' iconColor={personColor}/>
+      </TouchableOpacity>
     </View>
   );
 }
